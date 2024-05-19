dfa44d0545a026dd5e900f4a8ff0fcafc399c7e5
DavidCT9 <0253018@up.edu.mx>
2024-05-19 10:15:02 -0600

    Starting front-end

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-background">
+        {/* Background image is set in CSS, no need for img tag here */}
+
+        <div class="card">
+          <p>LOGOS HERE</p>
+        </div>
+
+        <div>
+          <button className="buttonpro">
+            <span> NEXT </span>
+          </button>
+        </div>
+
+
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
